d5d89e8fca10caad816551d5771703047bcce14d
Swati Pandey <swatipandeyhere@gmail.com>
2025-10-03 17:02:43 +0530

    Two-Way Communication

---
+++ named-pipes/chat2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/stat.h>
+
+#define BUF_SIZE 100
+
+char *read_fifo = "/tmp/fifo1";
+char *write_fifo = "/tmp/fifo2";
+
+void *read_messages(void *arg)
+{
+    char buf[BUF_SIZE];
+    int fd;
+
+    while (1)
+    {
+        fd = open(read_fifo, O_RDONLY);
+        if (fd < 0)
+            perror("open read_fifo");
+
+        read(fd, buf, BUF_SIZE);
+        printf("\nUser1: %s\nYou: ", buf);
+        fflush(stdout);
+        close(fd);
+    }
+}
+
+int main()
+{
+    pthread_t tid;
+
+    mkfifo(write_fifo, 0666);
+    mkfifo(read_fifo, 0666);
+
+    pthread_create(&tid, NULL, read_messages, NULL);
+
+    char buf[BUF_SIZE];
+    int fd;
+
+    while (1)
+    {
+        printf("You: ");
+        fgets(buf, BUF_SIZE, stdin);
+
+        fd = open(write_fifo, O_WRONLY);
+        if (fd < 0)
+            perror("open write_fifo");
+
+        write(fd, buf, strlen(buf) + 1);
+        close(fd);
+    }
+
+    pthread_join(tid, NULL);
+    return 0;
+}
